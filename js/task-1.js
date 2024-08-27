b421a39c015337f531fb0b28736c5c73d738939d
artem1925 <157005541+artem1925@users.noreply.github.com>
2024-08-27 09:02:08 +0200

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -5,3 +5,9 @@ function slugify(title) {
         .replace(/\s+/g, '-') // Замінюємо всі пробіли на тире
         .replace(/[^a-z0-9-]/g, ''); // Видаляємо всі символи, крім літер, цифр та тире
 }
+
+// Перевірка коректності роботи функції
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
